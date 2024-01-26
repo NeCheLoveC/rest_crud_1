c6a59a90f4db57e51c0e2b52676e953aef918ac4
Anton Protsenko <protseng@mail.ru>
2024-01-26 20:21:50 +0400

    Добавил Builder к сущности DepartmentCreateRequestDTO

--- src/main/java/com/example/springsecr/dto/model/request/department/DepartmentCreateRequestDTO.java
+++ src/main/java/com/example/springsecr/dto/model/request/department/DepartmentCreateRequestDTO.java
@@ -5,11 +5,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class DepartmentCreateRequestDTO
 {
     @NotBlank(message = "Имя пользователя не должно быть пустым")
